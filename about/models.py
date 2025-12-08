991def5d4dfb5752b810f2b86f7ff5bca7fbc7fe
sineadezita <s.ezita@icloud.com>
2025-12-08 21:49:29 +0000

    Add POST request from our collaboration form

--- about/models.py
+++ about/models.py
@@ -4,8 +4,19 @@ from django.db import models
 
 class About(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
     updated_on = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
 
     def __str__(self):
         return self.title
+
+
+
+class CollaborateRequest(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Collaboration request from {self.name}"
