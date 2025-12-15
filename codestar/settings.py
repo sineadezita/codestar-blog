1869e64c7020ce00774bb3697ba5afa70bd4db9b
sineadezita <s.ezita@icloud.com>
2025-12-15 22:37:56 +0000

    Add Whitenoise static files storage

--- codestar/settings.py
+++ codestar/settings.py
@@ -155,3 +155,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
